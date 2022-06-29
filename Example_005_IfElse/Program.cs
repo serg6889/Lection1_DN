f7d3b4c80f6d4829a539724bf80d0fe1dc7ad600
Sergey68 <sesergey@yandex.ru>
2022-06-29 21:16:54 +1100

    File is updated with Example_005_IfElse

---
+++ Example_005_IfElse/Program.cs
@@ -0,0 +1,14 @@
+﻿
+Console.WriteLine("Input username:  ");
+
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hurraaa !!! That is Masha !!!");
+}
+else
+{
+    Console.Write("Hello !");
+    Console.WriteLine(username);
+}
