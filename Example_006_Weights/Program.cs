37c88d306648331ec1647ada4664124c45174e59
Sergey68 <sesergey@yandex.ru>
2022-06-29 21:36:05 +1100

    File is uypdated with Example_006_Weights

---
+++ Example_006_Weights/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 3;
+int b = 5;
+int c = 7;
+int d = 9;
+int e = 1;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
